1b36c636b3b192e34d0194609679c013ab866ca0
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 10:46:05 +0530

    Practise counter_2 app

---
+++ Js_Practise/app.js
@@ -0,0 +1,14 @@
+//document.getElementById("count-el").innerText = 10
+
+// initialize the count as 0
+// listen for clicks on the increment button
+//increment the count variable when the button is called
+//change the count-el in the HTML to reflect the new count
+
+function methord(){
+    console.log(5)
+    console.log(4)
+}
+
+methord()
+methord()
