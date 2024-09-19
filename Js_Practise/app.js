5424b7db318d07e7295a7cce1e2909721b19c533
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-19 15:31:57 +0530

    practise again Js

--- Js_Practise/app.js
+++ Js_Practise/app.js
@@ -12,3 +12,27 @@ function methord(){
 
 methord()
 methord()
+
+
+let num1 = 12;
+let num2 = 23;
+let num3 = 34;
+
+function sum_num(){
+    let sum_let = num1 + num2 + num3;
+    console.log(sum_let)
+}
+
+sum_num()
+
+
+let lapsComplete = 0;
+
+function lap_com(){
+    lapsComplete=lapsComplete+1;
+    console.log(lapsComplete)
+}
+
+lap_com()
+lap_com()
+lap_com()
