7dccf6d2b82a9cd69046c76467d4aaed046fe948
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-21 16:00:24 +0530

    final count app

--- Js_Practise/app.js
+++ Js_Practise/app.js
@@ -101,3 +101,11 @@ function save(){
     console.log(count)
 }
 save()
+
+
+console.log("------------------------")
+
+console.log(4+5)
+console.log("2"+"4")
+console.log("5"+1)
+console.log(100+"100")
